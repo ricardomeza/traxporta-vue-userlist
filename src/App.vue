164f9819f5c7a98f13186524d74fd877fcf75d5b
Rico Meza <rico.meza.mx@gmail.com>
2023-12-05 22:01:40 -0600

    Add Template & Sidebar -tsconfig.app.json, vite.config.ts: add path alias -App.vue: add template, import & use Sidebar component -logo-traxporta.webp: add company logo -Sidebar.vue: add component

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,17 @@
   <header
     class="bg-gradient-to-r from-traxporta-green to-traxporta-blue w-full min-h-[5px]"
   ></header>
+
+  <!-- Main Content -->
+  <div class="flex flex-grow min-h-screen">
+    <!-- Sidebar -->
+    <Sidebar />
+
+    <!-- Content -->
+    <main class="flex-1 bg-gray-100 overflow-y-auto items-center justify-center"></main>
+  </div>
 </template>
+
+<script setup lang="ts">
+import Sidebar from '@components/Sidebar/Sidebar.vue'
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url))
     }
   }
 })
