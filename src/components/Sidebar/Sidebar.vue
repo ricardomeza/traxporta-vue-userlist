03a4ed672acb4bbed9f4f08142eb0b05f2920172
Rico Meza <rico.meza.mx@gmail.com>
2023-12-05 22:01:40 -0600

    Add Template & Sidebar -tsconfig.app.json, vite.config.ts: add path alias -App.vue: add template, import & use Sidebar component -logo-traxporta.webp: add company logo -Sidebar.vue: add component

---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <aside class="w-24 bg-white border-r border-gray-300">
+    <nav class="text-white text-base font-semibold pt-3">
+      <a href="#" class="flex items-center justify-center active-nav-link nav-item">
+        <img :src="logoImg" alt="icon" class="h-12 w-12 rounded-lg shadow-md" />
+      </a>
+    </nav>
+  </aside>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import logoImg from '@assets/logo-traxporta.webp'
+
+export default defineComponent({
+  name: 'side-bar',
+  setup() {
+    return { logoImg }
+  }
+})
+</script>
